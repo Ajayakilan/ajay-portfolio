dc298adfdc08ec939402ef8deb69a4cd280ac189
Ajayakilan <deanajay09@gmail.com>
2026-04-01 23:24:31 +0530

    Update Project

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import Headroom from "react-headroom";
 import "./Header.scss";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
@@ -6,7 +6,6 @@ import StyleContext from "../../contexts/StyleContext";
 import {
   greeting,
   skillsSection,
-  openSource,
   resumeSection,
   educationInfo,
   myProjects
@@ -14,69 +13,50 @@ import {
 
 function Header() {
   const {isDark} = useContext(StyleContext);
-  const viewOpenSource = openSource.display;
+  const [isOpen, setIsOpen] = useState(false);
   const viewSkills = skillsSection.display;
   const viewResume = resumeSection.display;
   const viewEducation = educationInfo.display;
   const viewMyProjects = myProjects.display;
 
-  return (
-    <Headroom>
-      <header className={isDark ? "dark-menu header" : "header"}>
-        <a href="/" className="logo">
-          <span className="grey-color"> &lt;</span>
-          <span className="logo-name">{greeting.username}</span>
-          <span className="grey-color">/&gt;</span>
-        </a>
-        <input className="menu-btn" type="checkbox" id="menu-btn" />
-        <label
-          className="menu-icon"
-          htmlFor="menu-btn"
-          style={{color: "white"}}
-        >
-          <span className={isDark ? "navicon navicon-dark" : "navicon"}></span>
-        </label>
-        <ul className={isDark ? "dark-menu menu" : "menu"}>
-          {viewSkills && (
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
-          )}
-          
-          {/* {viewOpenSource && (
-            <li>
-              <a href="#opensource">Open Source</a>
-            </li>
-          )} */}
-          {viewResume && (
-            <li>
-              <a href="#resume">Resume</a>
-            </li>
-          )}
+  const close = () => setIsOpen(false);
 
-          {viewEducation && (
-            <li>
-              <a href="#education">Education</a>
-            </li>
-          )}
-          {viewMyProjects && (
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-          )}
-          
-          <li>
-            <a href="#contact">Contact </a>
-          </li>
-          <li>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a>
-              <ToggleSwitch />
-            </a>
-          </li>
-        </ul>
-      </header>
-    </Headroom>
+  return (
+    <>
+      <Headroom>
+        <header className={isDark ? "dark-menu header" : "header"}>
+          <a href="/" className="logo">
+            <span className="grey-color"> &lt;</span>
+            <span className="logo-name">{greeting.username}</span>
+            <span className="grey-color">/&gt;</span>
+          </a>
+          <div className={`menu-icon ${isOpen ? "is-open" : ""}`} onClick={() => setIsOpen(p => !p)}>
+            <span className={isDark ? "navicon navicon-dark" : "navicon"}></span>
+          </div>
+          <ul className={isDark ? "dark-menu menu desktop-menu" : "menu desktop-menu"}>
+            {viewSkills && <li><a href="#skills">Skills</a></li>}
+            {viewResume && <li><a href="#resume">Resume</a></li>}
+            {viewEducation && <li><a href="#education">Education</a></li>}
+            {viewMyProjects && <li><a href="#projects">Projects</a></li>}
+            <li><a href="#contact">Contact</a></li>
+            <li><a><ToggleSwitch /></a></li>
+          </ul>
+        </header>
+      </Headroom>
+
+      {isOpen && (
+        <div className={`mobile-dropdown ${isDark ? "dark" : ""}`}>
+          <ul className="mobile-menu">
+            {viewSkills && <li><a href="#skills" onClick={close}>Skills</a></li>}
+            {viewResume && <li><a href="#resume" onClick={close}>Resume</a></li>}
+            {viewEducation && <li><a href="#education" onClick={close}>Education</a></li>}
+            {viewMyProjects && <li><a href="#projects" onClick={close}>Projects</a></li>}
+            <li><a href="#contact" onClick={close}>Contact</a></li>
+            <li><a><ToggleSwitch /></a></li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 export default Header;
